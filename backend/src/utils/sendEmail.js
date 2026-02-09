898c801e4665d496d57949aa032b93cf99d712ca
monisha-76 <monishamoorthy.16@gmail.com>
2026-02-09 14:50:01 +0530

    changed to sendGrid

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,23 +1,25 @@
-import { Resend } from "resend";
+import sgMail from "@sendgrid/mail";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const sendEmail = async (to, subject, html) => {
   try {
     if (!to) {
-      console.error("❌ Email sending failed: Recipient email missing");
+      console.error("❌ Email sending failed: Recipient missing");
       return;
     }
 
-    await resend.emails.send({
-      from: "Event Management <onboarding@resend.dev>", // works for testing
+    const msg = {
       to,
+      from: `${process.env.EMAIL_USER}`, 
       subject,
       html,
-    });
+    };
 
-    console.log("✅ Email sent successfully via Resend");
+    await sgMail.send(msg);
+
+    console.log("✅ Email sent successfully via SendGrid");
   } catch (err) {
-    console.error("❌ Email sending failed:", err);
+    console.error("❌ SendGrid email error:", err.response?.body || err.message);
   }
 };
